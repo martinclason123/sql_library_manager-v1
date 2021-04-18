62694a09c4de56b6c25723da09f96911500ca73f
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2021-04-18 06:47:38 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -13,10 +13,76 @@ router.get("/", (req, res, next) => {
   res.redirect('/books')
 })
 
+
+//search button route
+router.post("/search", async (req,res,next) => {
+  const searchString = req.body.search;
+  // redirects to home page in case of empty search
+  if(searchString === ""){
+    res.redirect("/books")
+  }
+  // searches the book db for any partial match accross all columns
+  const query = {
+    [Op.or] : {
+      title : {
+        [Op.like] : `%${searchString}%`
+      },
+      author : {
+        [Op.like] : `%${searchString}%`
+      },
+      genre : {
+        [Op.like] : `%${searchString}%`
+      },
+      year : {
+        [Op.like] : `%${searchString}%`
+      }
+    }
+  };
+  const books = await book.findAll({where: query,
+  limit: 10
+   
+  })
+  // provides the pagination links for the search results
+  let pagElements = await paginationArray({where: query});
+  res.render("search-results", { books, title: "Books", pagElements, searchString});
+})
+
+// pagination links route for search results
+router.get("/search/books/page/:page/:searchString", async (req,res,next) =>{
+  const searchString = req.params.searchString;
+  const query = {
+    [Op.or] : {
+      title : {
+        [Op.like] : `%${searchString}%`
+      },
+      author : {
+        [Op.like] : `%${searchString}%`
+      },
+      genre : {
+        [Op.like] : `%${searchString}%`
+      },
+      year : {
+        [Op.like] : `%${searchString}%`
+      }
+    }
+  };
+  let pagElements = await paginationArray({where: query});
+  // gymnastics to set offset based on link clicked
+  let startAt = (req.params.page - 1) * 10;
+  // finds the books starting at the offset, limited to 10 results
+  const books = await book.findAll({where: query,
+  offset : startAt,
+  limit: 10 
+  })
+  // retrieves the amount of pagination links to be displayed on page
+  res.render("search-results", { books, title: "Books", pagElements, searchString});
+})
+
+
 // function that returns the pagination element attributes
-const paginationArray = async () => {
+const paginationArray = async (query) => {
   // finds all rows
-  const {count, rows} = await book.findAndCountAll();
+  const {count, rows} = await book.findAndCountAll(query);
   //preforms gymnastics to get correct amount of pagination links
   let pagination =  Math.ceil(count / 10); 
   // creates an array of attributes to give to pug template
@@ -77,7 +143,6 @@ router.post("/books/new", async (req, res, next) => {
 // show books detail form
 router.get('/books/:id', async (req, res, next)=> {
    const bookDetails = await book.findByPk(req.params.id);
-   console.log(bookDetails)
    res.render('update-book', {bookDetails, title: bookDetails.title})
 })
 
